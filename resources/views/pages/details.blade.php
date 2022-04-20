805b346f155d4d9ad6d4af5ba950b9ea7476a68b
matteopelusi7 <pelusimatteo02@gmail.com>
2022-04-20 17:15:57 +0200

    Mod main

--- resources/views/pages/details.blade.php
+++ resources/views/pages/details.blade.php
@@ -33,7 +33,7 @@
                         </div>
                     </div>
                     <div>
-                        <p>{{ $comics['description'] }}</p>
+                        <p class="description">{{ $comics['description'] }}</p>
                     </div>
                 </div>
                 <div class="fig">
@@ -48,8 +48,48 @@
 
         <div class="container-comics-2">
 
-            <div class="container">
-                
+            <div class="container-b">
+
+                <div class="cont">
+                    <h3>Talent</h3>
+                    <div>
+                        <div class="aut">
+                            <p class="aut-1">Art by:</p>
+                            <div class="aut-3">
+                                @foreach( $comics['artists'] as $comic )
+                                    <p class="lightblue">{{ $comic }}, </p>
+                                @endforeach
+                            </div>
+                        </div>
+                        <div class="aut">
+                            <p class="aut-1">Written by:</p>
+                            <div class="aut-3">
+                                @foreach( $comics['writers'] as $comic )
+                                    <p class="lightblue">{{ $comic }}, </p>
+                                @endforeach
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="cont">
+                    <h3>Specs</h3>
+                    <div>
+                        <div class="aut">
+                            <p class="aut-1">Series</p>
+                            <p class="lightblue">{{ $comics['series'] }}</p>
+                        </div>
+                        <div class="aut">
+                            <p class="aut-1">U.S. Price:</p>
+                            <p>{{ $comics['price'] }}</p>
+                        </div>
+                        <div class="aut">
+                            <p class="aut-1">On Sale Date:</p>
+                            <p>{{ $comics['sale_date'] }}</p>
+                        </div>
+                    </div>
+                </div>
+
             </div>
 
         </div>
